09b01b5952bd35a17b71474a204be913b037f49a
gouthaman001 <ch16m027@smail.iitm.ac.in>
2016-10-22 20:09:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.get('/', function (req, res) {
 
 
 app.get('/article-one',function(req,res){
-    res.send("article one is asked and you will never be able to get it even in your dreams")
-}
+    res.send("article one is asked and you will never be able to get it even in your dreams");
+});
 app.get('/article-two',function(req,res){
-    res.send("article two  is asked and still no luck buddy")
-}
+    res.send("article two  is asked and still no luck buddy");
+});
 app.get('/article-three',function(req,res){
-    res.send("article three is asked but where the hell is it??")
-}
+    res.send("article three is asked but where the hell is it??");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
