e99b60ee8de911827462a918dd44278ac34c0d5f
gouthaman001 <ch16m027@smail.iitm.ac.in>
2016-11-05 10:31:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,7 @@
 console.log('Loaded!');
-var madi=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+5;
-    madi.style.marginLeft=marginLeft+'px';
-}
-madi.onclick=function(){
-    var interval=setInterval(moveRight,50);
-    
+var element=document.getElementbyId('main-text');
+element.innerHTML='new value';
+var img=document.getElementbyId('madi');
+img.onclick=function(){
+    img.style.marginleft='100px';
 };
